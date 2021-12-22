f829f1547cf8f7e4dc35ee65760779c0e55e5735
liuhang <liuhang@transfereasy.com>
2021-12-22 18:53:48 +0800

    update

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import websockets
 import aioredis
 
 import logging
+import async_timeout
 
 logging.basicConfig(
     format="%(asctime)s %(message)s",
@@ -13,6 +14,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger("websockets.client")
 
+STOPWORD = "STOP"
 
 async def consumer_handler(websocket, redis, user):
     async for message in websocket:
@@ -20,28 +22,39 @@ async def consumer_handler(websocket, redis, user):
         await redis.lpush('client_commands', command )
 
 async def producer_handler(websocket, ch):
-    while await ch.wait_message():
-            msg = await ch
-            await websocket.send(msg)
+    while True:
+        try:
+            async with async_timeout.timeout(1):
+                message = await ch.get_message(ignore_subscribe_messages=True)
+                if message is not None:
+                    print(f"(Reader) Message Received: {message}")
+                    if message["data"].decode() == STOPWORD:
+                        print("(Reader) STOP")
+                        break
+                    await websocket.send(message['data'])
+                await asyncio.sleep(0.01)
+        except asyncio.TimeoutError:
+            pass
 
 connected = set()
 
 async def handler(websocket):
     token = await websocket.recv()
-    sub = await aioredis.from_url('redis://localhost')
-    pub = await aioredis.from_url('redis://localhost')
+    consumer_redis = await aioredis.from_url('redis://localhost')
+    pub_redis = await aioredis.from_url('redis://localhost')
 
-    user = await sub.get(token)
+    user = await consumer_redis.get(token)
     if user is None:
         await websocket.close(1011, "authentication failed")
         return
     user = user.decode('utf8')
-    ch, *_ = await pub.subscribe('chan:'+'user')
+    pubsub = pub_redis.pubsub()
+    await pubsub.subscribe("channel:"+user)
 
     connected.add(websocket)
     await asyncio.gather(
-        consumer_handler(websocket, sub, user),
-        producer_handler(websocket, ch),
+        consumer_handler(websocket, consumer_redis, user),
+        producer_handler(websocket, pubsub),
     )
 
 async def main():
